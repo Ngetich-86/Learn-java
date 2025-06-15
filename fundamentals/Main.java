829d4479810bd1296d942700e0eb8008a461068c
Ngetich-86 <gideonkngetich86@gmail.com>
2025-06-15 13:55:43 +0300

    learning java

---
+++ fundamentals/Main.java
@@ -0,0 +1,22 @@
+// package fundamentals;
+
+// public class Main{
+//     public static void main(String[] args){
+//         System.out.println(3 + 3);
+//         System.out.println("Welcome to Java programming.");
+//     }
+// }
+
+//======================data types================
+// package fundamentals;
+// public class Main {
+//     public static void main(String[] args) {
+//         String name = "Ngetich";
+//         int age = 22;
+//         boolean status = false;
+//         System.out.println("Name: " + name + ", Age: " + age + ", Married: " + status);
+//     }
+// }
+
+//====================Numbers==> bit, short,long, double, float
+
