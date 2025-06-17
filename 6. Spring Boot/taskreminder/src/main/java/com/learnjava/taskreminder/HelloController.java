a4de7f06da47d708ae1758afd4d40ca474f51893
Ngetich-86 <gideonkngetich86@gmail.com>
2025-06-17 14:44:15 +0300

    run my first hello world in springboot

---
+++ 6. Spring Boot/taskreminder/src/main/java/com/learnjava/taskreminder/HelloController.java
@@ -0,0 +1,20 @@
+// package com.learnjava.taskreminder;
+
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// @RestController
+// public class HelloController {
+
+//     @GetMapping("/")
+//     public String hello() {
+//         return "Hello World!";
+//     }
+// }
+
+package com.learnjava.taskreminder;
+public class HelloController {
+    public String hello() {
+        return "Hello World!";
+    }
+}
