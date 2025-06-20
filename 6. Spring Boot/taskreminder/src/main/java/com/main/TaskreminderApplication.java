64c7ca1250a807441ae78815f9d0ca1f905ed9fb
Ngetich-86 <gideonkngetich86@gmail.com>
2025-06-20 11:44:37 +0300

    feat: added dotenv for securing envs

--- 6. Spring Boot/taskreminder/src/main/java/com/main/TaskreminderApplication.java
+++ 6. Spring Boot/taskreminder/src/main/java/com/main/TaskreminderApplication.java
@@ -3,12 +3,15 @@ package com.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @EnableCaching
 @SpringBootApplication
 public class TaskreminderApplication {
 
 	public static void main(String[] args) {
+		// Load .env file for local development
+		Dotenv.configure().ignoreIfMissing().load();
 		SpringApplication.run(TaskreminderApplication.class, args);
 	}
 
