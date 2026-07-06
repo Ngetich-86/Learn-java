e021761ee7c048ce89315946a9e1c74f40f65e95
Ngetich-86 <gideonkngetich86@gmail.com>
2026-07-06 07:16:52 -0600

    fix on MergeSortedLinkedList

--- DSA-Practice/02_Linked_Lists/13_Merge_Sorted_Linked_Lists.java
+++ DSA-Practice/02_Linked_Lists/13_Merge_Sorted_Linked_Lists.java
@@ -30,34 +30,84 @@
 //     tail.next = list2
 // RETURN dummy.next
 // ==========================================code ================================
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
 
 class MergeSortedLinkedList {
-    public static ListNode MergeLinkedList(ListNode list1, ListNode list2) {
-        // create a dummy
+    public static ListNode mergeLinkedList(ListNode list1, ListNode list2) {
+        // Dummy node
         ListNode dummy = new ListNode(-1);
-        //Tail always points to the last node in the merged list
+
+        // Tail pointer
         ListNode tail = dummy;
-        // Compare nodes while both lists still have elements
-        while(list1 != null && list2 != null){
-            if(list1.value <= list2.value){
+
+        while (list1 != null && list2 != null) {
+
+            if (list1.val <= list2.val) {
                 tail.next = list1;
                 list1 = list1.next;
-            }
-            else {
+            } else {
                 tail.next = list2;
                 list2 = list2.next;
             }
-            // Move tail forward
+
             tail = tail.next;
         }
-        // attach whatever remains
-        if(list1 != null) {
+
+        // Attach remaining nodes
+        if (list1 != null) {
             tail.next = list1;
-        }
-        else {
+        } else {
             tail.next = list2;
         }
-        // return merged list
+
         return dummy.next;
+    }
+
+    // Helper method to print a linked list
+    public static void printList(ListNode head) {
+
+        while (head != null) {
+            System.out.print(head.val);
+
+            if (head.next != null) {
+                System.out.print(" -> ");
+            }
+
+            head = head.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        // Create first list: 1 -> 3 -> 5
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(3);
+        list1.next.next = new ListNode(5);
+
+        // Create second list: 2 -> 4 -> 6
+        ListNode list2 = new ListNode(2);
+        list2.next = new ListNode(4);
+        list2.next.next = new ListNode(6);
+
+        System.out.println("List 1:");
+        printList(list1);
+
+        System.out.println("List 2:");
+        printList(list2);
+
+        ListNode merged = mergeLinkedList(list1, list2);
+
+        System.out.println("Merged List:");
+        printList(merged);
     }
 }
